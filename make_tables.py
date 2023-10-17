a9cff3c0bfa5b8c79a8955f46992170da444eac8
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2023-10-17 17:16:02 +1100

    put namelists in ignored subdir

--- make_tables.py
+++ make_tables.py
@@ -3,12 +3,16 @@
 # Update latex tables of namelists
 
 import nmltab  # from https://github.com/aekiss/nmltab
+import os
+
+outdir = 'namelists'
+configdir = 'configs'
 
 nmls = ['ACCESS-OM2_1deg_jra55_ryf/ice/cice_in.nml',
         'MOM6-CICE6_1deg_jra55do_ryf/ice_in']
-
+nmls = [ os.path.join(configdir, d) for d in nmls ]
 texfname = 'cice_nml.tex'
 st = nmltab.strnmldict(nmltab.nmldict(nmls), fmt='latex')
-with open(texfname, 'w') as f:
+with open(os.path.join(outdir, texfname), 'w') as f:
     f.write(st)
 print('   {}'.format(texfname))
