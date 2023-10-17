dcc85f55183d568b1d98d92be8e6e0cb41e6fe1f
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2023-10-17 17:25:15 +1100

    create initial namelists dir

--- make_tables.py
+++ make_tables.py
@@ -5,14 +5,15 @@
 import nmltab  # from https://github.com/aekiss/nmltab
 import os
 
-outdir = 'namelists'
-configdir = 'configs'
+configs = 'configs'
+namelists = 'namelists'
+os.makedirs(namelists, exist_ok=True)
 
 nmls = ['ACCESS-OM2_1deg_jra55_ryf/ice/cice_in.nml',
         'MOM6-CICE6_1deg_jra55do_ryf/ice_in']
-nmls = [ os.path.join(configdir, d) for d in nmls ]
+nmls = [ os.path.join(configs, d) for d in nmls ]
 texfname = 'cice_nml.tex'
 st = nmltab.strnmldict(nmltab.nmldict(nmls), fmt='latex')
-with open(os.path.join(outdir, texfname), 'w') as f:
+with open(os.path.join(namelists, texfname), 'w') as f:
     f.write(st)
 print('   {}'.format(texfname))
