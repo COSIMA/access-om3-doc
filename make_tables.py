6eb0d5e016d5dd09e10750086433e0232e3dd265
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2023-10-26 11:00:42 +1100

    update

--- make_tables.py
+++ make_tables.py
@@ -9,12 +9,38 @@ configs = 'configs'
 namelists = 'namelists'
 os.makedirs(namelists, exist_ok=True)
 
-nmls = [#'ACCESS-OM2_1deg_jra55_ryf/ocean/input.nml',
+os.chdir(configs)
+
+def savetables(nmls, fname, url):
+    print('   {}'.format(fname))
+    # nmls = [ os.path.join(configs, d) for d in nmls ]
+    st = nmltab.strnmldict(nmltab.nmldict(nmls), fmt='latex')
+    with open(os.path.join('..', namelists, fname+'_nml.tex'), 'w') as f:
+        f.write(st)
+    st = nmltab.strnmldict(nmltab.nmldict(nmls), fmt='markdown2', url=url)
+    with open(os.path.join('..', namelists, fname+'_nml.md'), 'w') as f:
+        f.write(st)
+    st = nmltab.strnmldict(nmltab.nmldict(nmls), fmt='csv', url=url)
+    with open(os.path.join('..', namelists, fname+'_nml.csv'), 'w') as f:
+        f.write(st)
+  
+savetables( [#'ACCESS-OM2_1deg_jra55_ryf/ocean/input.nml',
         'MOM6-examples/ocean_only/global/MOM_input',
         'mom6-om4-025/MOM_input',
         'mom6-panan/MOM_input',
         'mom6-eac/MOM_input',
-        'MOM6-CICE6_1deg_jra55do_ryf/MOM_input']
+        'MOM6-CICE6_1deg_jra55do_ryf/MOM_input'],
+        'mom',
+        'https://github.com/mom-ocean/MOM6/search?q=')
+
+savetables(['ACCESS-OM2_1deg_jra55_ryf/ice/cice_in.nml',
+        'MOM6-CICE6_1deg_jra55do_ryf/ice_in'],
+        'cice',
+        'https://cice-consortium-cice.readthedocs.io/en/main/search.html?q='
+        )
+
+quit()
+
 nmls = [ os.path.join(configs, d) for d in nmls ]
 texfname = 'mom_nml.tex'
 st = nmltab.strnmldict(nmltab.nmldict(nmls), fmt='latex')
