05175509754dbc8b48d21de9849c1081b1dc104f
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2023-10-31 17:33:50 +1100

    update tables

--- make_tables.py
+++ make_tables.py
@@ -90,6 +90,15 @@ savetables([
         'https://cice-consortium-cice.readthedocs.io/en/main/search.html?q='
         )
 
+savetables([
+        'MOM6-CICE6_cice_config/ice_in',
+        'ACCESS-OM2_1deg_jra55_ryf/ice/cice_in.nml',
+        'MOM6-CICE6_1deg_jra55do_ryf/ice_in',
+        ],
+        'ice_in/MOM6-CICE6_cice_config',
+        'https://cice-consortium-cice.readthedocs.io/en/main/search.html?q='
+        )
+
 savetables( [
         'MOM6-CICE6_1deg_jra55do_ryf/MOM_input',
         'MOM6-CICE6_1deg_jra55do_iaf/MOM_input',
@@ -121,6 +130,13 @@ savetables( [
         'MOM6-examples/ocean_only/global/MOM_input',
         ],
         'MOM_input/mom6_global',
+        'https://github.com/mom-ocean/MOM6/search?q=')
+
+savetables( [
+        'MOM6-CICE6_iss83_MOM_input_update/MOM_input',
+        'mom6-om4-025/MOM_input',
+        ],
+        'MOM_input/MOM6-CICE6_iss83_MOM_input_update',
         'https://github.com/mom-ocean/MOM6/search?q=')
 
 savetables( [
